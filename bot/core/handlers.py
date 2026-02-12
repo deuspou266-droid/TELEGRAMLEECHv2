47bbae564bea54eb028d5c2eaa2366a3bd27ae68
deuspou266-droid <deuspou266@gmail.com>
2026-02-12 21:39:47 +0000

    adiciona manipuladores para comandos Spotdl e SpotdlLeech ao bot

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -378,6 +378,20 @@ def add_handlers():
     )
     TgClient.bot.add_handler(
         MessageHandler(
+            spotdl,
+            filters=command(BotCommands.SpotdlCommand, case_sensitive=True)
+            & CustomFilters.authorized,
+        )
+    )
+    TgClient.bot.add_handler(
+        MessageHandler(
+            spotdl_leech,
+            filters=command(BotCommands.SpotdlLeechCommand, case_sensitive=True)
+            & CustomFilters.authorized,
+        )
+    )
+    TgClient.bot.add_handler(
+        MessageHandler(
             hydra_search,
             filters=command(BotCommands.NzbSearchCommand, case_sensitive=True)
             & CustomFilters.authorized,
--- bot/modules/__init__.py
+++ bot/modules/__init__.py
@@ -38,6 +38,7 @@ from .stats import bot_stats, stats_pages, get_packages_version
 from .status import task_status, status_pages
 from .users_settings import get_users_settings, edit_user_settings, send_user_settings
 from .ytdlp import ytdl, ytdl_leech
+from .spotdl import spotdl, spotdl_leech
 from .mangaleech import (
     mangaleech,
     manga_source_callback,
@@ -115,4 +116,6 @@ __all__ = [
     "manga_mode_callback",
     "manga_result_callback",
     "manga_message_handler",
+    "spotdl",
+    "spotdl_leech",
 ]
